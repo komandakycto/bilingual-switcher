25cc1398da1d0cf57ec52620f04d2374b906000d
Leonid Korsakov <artforme2011@gmail.com>
2026-04-29 06:46:43 +0000

    Update Casks/bilingual-switcher.rb for 1.0.1 [skip ci]

--- Casks/bilingual-switcher.rb
+++ Casks/bilingual-switcher.rb
@@ -1,8 +1,8 @@
 cask "bilingual-switcher" do
-  version "1.0.0"
-  sha256 "61a662030ada330614e75c59cd36f7ec20e4037c504f7da7e7def2a51027bcdc"
+  version "1.0.1"
+  sha256 "e2d218bb5db8a3943e99d09d1c411227831e83999bd34551bf1ad5cb8c55bb3d"
 
-  url "https://github.com/komandakycto/bilingual-switcher/releases/download/v1.0.0/BilingualSwitcher.zip"
+  url "https://github.com/komandakycto/bilingual-switcher/releases/download/v1.0.1/BilingualSwitcher.zip"
   name "Bilingual Switcher"
   desc "Convert selected text between keyboard layouts with a hotkey"
   homepage "https://github.com/komandakycto/bilingual-switcher"
